81be7c714739461d862ae9c758e81908a6756436
sinida <shinetrain01@gmail.com>
2016-08-30 11:20:36 +0800

    [bug/feature] init file

---
+++ Testcodeonjen_vs2015/main.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+
+void main() {
+
+	cout << "Hi VS" << endl;
+
+	system("pause");
+}
---
+++ Testcodeonjen_vs2015/main.h
@@ -0,0 +1,8 @@
+#pragma once
+/*-----------------------------------------
+Create date: 2016/08/30 10:55 UTC-8
+update: 
+-----------------------------------------*/
+#include <iostream>
+
+using namespace std;
